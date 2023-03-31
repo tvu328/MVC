d1028754da56d1219a183b95ec1c10955f82b1bd
Tyler <tylerlvvu@gmail.com>
2023-03-31 00:29:35 -0700

    adding console log

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -10,7 +10,7 @@ const commentFormHandler = async event => {
     ];
   
     if (comment_text) {
-      const response = await fetch("/api/comments", {
+      const response = await fetch("/api/comment-routes", {
         method: "POST",
         body: JSON.stringify({
           postId,
